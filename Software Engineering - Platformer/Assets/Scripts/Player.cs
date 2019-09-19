e103982b37e0b27fbd930897c8c9bcf464bdfa4a
Tyler Ramey <speto61@gmail.com>
2019-09-19 13:32:14 -0400

    Inital Commit
    The fist commit of the whole game

---
+++ Software Engineering - Platformer/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float pSpeed = 5f;
+    public float jumpSpeed = 5f;
+
+    public bool alive = true;
+    public bool onGround = true;
+
+    public Transform groundedChecker;
+    public float checkGroundR;
+    public LayerMask layer_ground;
+
+    Rigidbody2D rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        float movex = Input.GetAxisRaw("Horizontal");
+        float move = movex * pSpeed;
+        rb.velocity = new Vector2(move, rb.velocity.y);
+
+        checkForGround();
+
+        if (onGround) {
+            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space)) {
+                rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+            }
+        }
+    }
+
+    void checkForGround() {
+        Collider2D collider = Physics2D.OverlapCircle(groundedChecker.position, checkGroundR, layer_ground);
+        if (groundedChecker != null) {
+            onGround = true;
+        }
+        else {
+            onGround = false;
+        }
+    }
+}
