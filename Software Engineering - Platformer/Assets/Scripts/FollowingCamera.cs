0dc487a11c4689420be248119ae78bf13bc9ded2
Tyler Ramey <speto61@gmail.com>
2019-09-23 08:04:41 -0400

    Camera Follow

---
+++ Software Engineering - Platformer/Assets/Scripts/FollowingCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingCamera : MonoBehaviour {
+    public Transform player;
+    public Vector3 offset;
+
+    void Start() {
+        Debug.Log("!Camera Follow!");
+    }
+
+    void Update() {
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z);
+    }
+}
