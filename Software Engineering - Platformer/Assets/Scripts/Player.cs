b598d2dc17bceca548a5788563fc4a7eb38d4b00
Alan Zhan <55457750+ayz975@users.noreply.github.com>
2019-10-02 08:17:12 -0400

    Grvity test

--- Software Engineering - Platformer/Assets/Scripts/Player.cs
+++ Software Engineering - Platformer/Assets/Scripts/Player.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    
     public float pSpeed = 5f;
     public float jumpSpeed = 5f;
+    public float distance;
+    public float radius;
+    public float gravity;
 
     public bool alive = true;
     public bool onGround = false;
+    public bool nearPlanet = false;
+    public bool touchingPlanet = false;
 
     public int jumped = 0;
 
@@ -16,28 +22,51 @@ public class Player : MonoBehaviour {
     public Transform playerTransform;
     public GameObject player;
     public GameObject startPos;
-
+    public Transform planet;
     void Start() {
         rb = GetComponent<Rigidbody2D>();
     }
 
     void Update() {
+        Physics.gravity = new Vector3(1f, -9.81f, 0f);
         float movex = Input.GetAxisRaw("Horizontal");
         float move = movex * pSpeed;
         rb.velocity = new Vector2(move, rb.velocity.y);
 
-        if (onGround) {
+        if (onGround && ! touchingPlanet) {
             if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)) {
                 rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
                 jumped++;
             }
         }
-        if (Input.GetKeyDown(KeyCode.R)) {
+        if (touchingPlanet && onGround){
+            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+            {
+                AwayTarget(planet, distance, gravity);
+                jumped++;
+            }
+        }
+        if (nearPlanet)
+        {
+            FollowTarget(planet, distance, gravity);
+        }
+            if (Input.GetKeyDown(KeyCode.R)) {
             player.transform.localPosition = new Vector2(startPos.transform.position.x, startPos.transform.position.y); 
         }
+        else { }
 
         if (player.transform.position.y < -10) {
             player.transform.localPosition = new Vector2(startPos.transform.position.x, startPos.transform.position.y);
+        }
+        distance = Vector2.Distance(planet.transform.position, player.transform.position);
+        if (distance > radius)
+        {
+            nearPlanet = false;
+
+        }
+        if (radius >= distance)
+        {
+            nearPlanet = true;
         }
     }
 
@@ -55,6 +84,25 @@ public class Player : MonoBehaviour {
         onGround = false;
     }
 
+    void FollowTarget(Transform target, float distanceToStop, float speed)
+    {
+        var direction = Vector3.zero;
+        if (Vector3.Distance(transform.position, target.position) > distanceToStop)
+        {
+            direction = target.position - transform.position;
+            rb.AddRelativeForce(direction.normalized * speed, ForceMode2D.Force);
+        }
+    }
+    void AwayTarget(Transform target, float distanceToStop, float speed)
+    {
+        var direction = Vector3.zero;
+        if (Vector3.Distance(transform.position, target.position) > distanceToStop)
+        {
+            direction = transform.position - target.position;
+            rb.AddRelativeForce(direction.normalized * speed, ForceMode2D.Force);
+        }
+    }
+
     private void CheckIfGrounded() {
         RaycastHit2D[] hits;
         
@@ -63,6 +111,13 @@ public class Player : MonoBehaviour {
         
         if (hits.Length > 0) {
             onGround = true;
+            if (nearPlanet)
+            {
+                touchingPlanet = true;
+            }
+            else { touchingPlanet = false; }
         }
     }
+
+
 }
