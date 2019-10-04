e47ef1896210a913c0d3b621aea112acfc632949
Tyler Ramey <speto61@gmail.com>
2019-10-04 13:29:55 -0400

    Enemy work begins

---
+++ Software Engineering - Platformer/Assets/Scripts/Red_Bird.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Red_Bird : MonoBehaviour {
+    public float moveSpeed = 5f;
+    public int hp = 1;
+
+    
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if(hp >= 0) { //death
+            Destroy(this);
+        }
+	}
+}
