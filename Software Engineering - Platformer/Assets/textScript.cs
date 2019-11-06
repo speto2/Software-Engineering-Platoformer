8f69e382dd1a209a441646cdcb6c1e0165ad6cd1
ayz975 <ayz975@gmail.com>
2019-11-06 10:04:21 -0500

    text
    d

---
+++ Software Engineering - Platformer/Assets/textScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class textScript : MonoBehaviour {
+    public Text canvasText1;
+    void Start()
+    {
+        Invoke("DisableText", 5f);//invoke after 5 seconds
+    }
+    void DisableText()
+    {
+        canvasText1.enabled = false;
+    }
+}
