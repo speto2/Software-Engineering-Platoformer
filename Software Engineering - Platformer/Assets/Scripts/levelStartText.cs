9eee8ee51911a9fca51084ccbd7a8855cccc1c48
Tyler Ramey <speto61@gmail.com>
2019-11-07 10:38:56 -0500

    text script fixed

--- Software Engineering - Platformer/Assets/Scripts/levelStartText.cs
+++ Software Engineering - Platformer/Assets/Scripts/levelStartText.cs
@@ -1,24 +1,21 @@
-﻿   using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class levelStartText : MonoBehaviour {
     public GUIText WhateverTextThingy;  //Add reference to UI Text here via the inspector
     private float timeToAppear = 2f;
     private float timeWhenDisappear;
 
     //Call to enable the text, which also sets the timer
-    public void EnableText()
-    {
+    public void EnableText() {
         WhateverTextThingy.enabled = true;
         timeWhenDisappear = Time.time + timeToAppear;
     }
 
     //We check every frame if the timer has expired and the text should disappear
-    void Update()
-    {
-        if (WhateverTextThingy.enabled && (Time.time >= timeWhenDisappear))
-        {
+    void Update() {
+        if (WhateverTextThingy.enabled && (Time.time >= timeWhenDisappear)) {
             WhateverTextThingy.enabled = false;
         }
     }
