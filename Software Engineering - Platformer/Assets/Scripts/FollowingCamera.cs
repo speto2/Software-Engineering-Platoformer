e849a9b8cce47bbabd8629c30f039493e20e4202
ayz975 <ayz975@gmail.com>
2019-11-08 18:11:55 -0500

    Smooth Following Camera
    made the following camera more smooth and less jerky

--- Software Engineering - Platformer/Assets/Scripts/FollowingCamera.cs
+++ Software Engineering - Platformer/Assets/Scripts/FollowingCamera.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowingCamera : MonoBehaviour {
-    public GameObject player;        
+public class FollowingCamera : MonoBehaviour {       
+    public Transform player;
     private Vector3 offset;
+    private Vector3 velocity = Vector3.zero;
+    public float smoothSpeed = 0.125f;
 
     void Start() {
         offset = transform.position - player.transform.position;
     }
+    
 
-    void LateUpdate() {
-        transform.position = player.transform.position + offset;
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = player.position + offset;
+        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed);
+        transform.position = smoothedPosition;
     }
 }
