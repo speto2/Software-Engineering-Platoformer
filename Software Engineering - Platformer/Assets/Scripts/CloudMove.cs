eaa37b476f20dd474377d8e5967bd6f68dda68a5
ayz975 <ayz975@gmail.com>
2019-11-07 23:05:10 -0500

    cloud wip
    cloud

---
+++ Software Engineering - Platformer/Assets/Scripts/CloudMove.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMove : MonoBehaviour {
+    public GameObject bg;
+    Rigidbody2D rb;
+
+    public int speed = 5;
+    private float minXValueWorld;
+    private float maxXValueWorld;
+
+    void Start()
+    {
+        minXValueWorld = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)).x;
+        maxXValueWorld = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x;
+    }
+
+    void Move()
+    {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            this.transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            this.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+    }
+
+    void OffscreenCheck()
+    {
+
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        Vector3 maxWorldXWithPlayerSize = Camera.main.WorldToScreenPoint(new Vector3(maxXValueWorld, 0, 0));
+        Vector3 minWorldWithPlayerSize = Camera.main.WorldToScreenPoint(new Vector3(minXValueWorld, 0, 0));
+
+        if (screenPos.x < minWorldWithPlayerSize.x)
+        {
+            this.transform.position = new Vector3(maxWorldXWithPlayerSize.x, this.transform.position.y, this.transform.position.z);
+        }
+
+        if (screenPos.x > maxWorldXWithPlayerSize.x)
+        {
+            this.transform.position = new Vector3(minWorldWithPlayerSize.x, this.transform.position.y, this.transform.position.z);
+        }
+        screenPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        Vector3 maxWorldWithPlayerSize = new Vector3(maxXValueWorld/ 2, 0, 0);
+        minWorldWithPlayerSize = new Vector3(minXValueWorld/ 2, 0, 0);
+        Vector3 maxScreenWithPlayerSize = Camera.main.WorldToScreenPoint(maxWorldWithPlayerSize);
+        Vector3 minScreenWithPlayerSize = Camera.main.WorldToScreenPoint(minWorldWithPlayerSize);
+
+        if (screenPos.x < minScreenWithPlayerSize.x)
+        {
+            this.transform.position = new Vector3(
+                maxWorldWithPlayerSize.x,
+                this.transform.position.y,
+                this.transform.position.z);
+        }
+
+        if (screenPos.x > maxScreenWithPlayerSize.x)
+        {
+            this.transform.position = new Vector3(
+                minWorldWithPlayerSize.x,
+                this.transform.position.y,
+                this.transform.position.z);
+        }
+
+
+
+    }
+
+    void update()
+    {
+        OffscreenCheck();
+
+    }
+}
---
+++ Software Engineering - Platformer/Assets/Scripts/eFollow.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class efollow : MonoBehaviour
+{
+    public Transform target;
+    public Transform mytransform;
+    public GameObject enemy;
+    public float elife = 100f;
+    GameObject ebullet;
+
+    // Use this for initialization
+    void Start()
+    {
+        ebullet = Resources.Load("bullets") as GameObject;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+        transform.Translate(Vector3.forward * 5 * Time.deltaTime);
+
+        GameObject bullets = Instantiate(ebullet) as GameObject;
+        bullets.transform.position = transform.position * 1;
+
+        Rigidbody rb = bullets.GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * 20;
+        Destroy(bullets, 9f);
+
+        if (elife <= 0)
+        {
+            enemy.SetActive(false);
+        }
+
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("bulleter"))
+        {
+            //  other.gameObject.SetActive(false);
+            elife -= 50;
+        }
+    }
+
+}
