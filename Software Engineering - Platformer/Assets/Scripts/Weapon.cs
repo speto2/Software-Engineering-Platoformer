4c5074d1edc5d10696240c6c071a38296eb6eb34
ayz975 <ayz975@gmail.com>
2019-11-09 16:27:45 -0500

    2 gun
    added a raycast based gun
    Switch weapons with k

--- Software Engineering - Platformer/Assets/Scripts/Weapon.cs
+++ Software Engineering - Platformer/Assets/Scripts/Weapon.cs
@@ -7,14 +7,49 @@ public class Weapon : MonoBehaviour {
 
     public Transform firePoint;
     public GameObject bulletPrefab;
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetButtonDown("Fire1")) {
+    public bool isPistol;
+    public GameObject impactEffect;
+    public LineRenderer lineRenderer;
+
+    void Start() {
+        isPistol = true;
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && isPistol)
+        {
             Shoot();
         }
-	}
+        if (Input.GetButtonDown("Fire1") && !isPistol) {
+            StartCoroutine(rayCast());
+        }
+        if (Input.GetButtonDown("Fire2")) {
+            isPistol = !isPistol;
+        }
+    }
 
     void Shoot(){
         Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+
+    IEnumerator rayCast() {
+        RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right);
+
+        if (hitInfo)
+        {
+            //Instantiate(impactEffect, hitInfo.point, Quaternion.identity);
+
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, hitInfo.point);
+        }else{
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, firePoint.position + firePoint.right * 100);
+        }
+
+        lineRenderer.enabled = true;
+
+        yield return 0;
+
+        lineRenderer.enabled = false;
     }
 }
