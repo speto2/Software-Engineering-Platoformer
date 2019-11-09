f7787fc1ef04565e9d2ea5c75fe8b5cf15fd6d60
ayz975 <ayz975@gmail.com>
2019-11-08 19:39:15 -0500

    Added Shadows (testing change)
    testing this change. makes it look kinda 3d which is cool

---
+++ Software Engineering - Platformer/Assets/Scripts/Shadow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shadow : MonoBehaviour {
+    public Vector2 offset = new Vector2(-0.1f, -0.1f);
+
+    private SpriteRenderer sprRndCaster;
+    private SpriteRenderer sprRndShadow;
+
+    private Transform transCaster;
+    private Transform transShadow;
+
+    public Material shadowMaterial;
+    public Color shadowColor;
+
+	void Start () {
+
+        transCaster = transform;
+        transShadow = new GameObject().transform;
+        transShadow.parent = transCaster;
+        transShadow.gameObject.name = "shadow";
+        transShadow.localRotation = Quaternion.identity;
+
+        sprRndCaster = GetComponent<SpriteRenderer>();
+        sprRndShadow = transShadow.gameObject.AddComponent<SpriteRenderer>();
+
+        sprRndShadow.material = shadowMaterial;
+        sprRndShadow.color = shadowColor;
+        sprRndShadow.sortingLayerName = sprRndCaster.sortingLayerName;
+        sprRndShadow.sortingOrder = sprRndCaster.sortingOrder - 1;
+
+
+
+
+	}
+	
+
+	void LateUpdate () {
+
+        transShadow.position = new Vector2(transCaster.position.x + offset.x, transCaster.position.y + offset.y);
+
+        sprRndShadow.sprite = sprRndCaster.sprite;
+
+	}
+}
