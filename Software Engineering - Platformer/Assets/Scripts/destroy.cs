9735e0b8d45f89f90376d02def3bda09e4275992
ayz975 <ayz975@gmail.com>
2019-11-13 13:41:32 -0500

    destroy
    ddddddnot working

---
+++ Software Engineering - Platformer/Assets/Scripts/destroy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class destroy : MonoBehaviour {
+    public int time = 20;
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, time);
+	}
+}
