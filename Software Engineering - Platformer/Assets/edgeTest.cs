dcf502ddf51106bdfd5d0e221756796a3b7c4858
Tyler Ramey <speto61@gmail.com>
2019-11-18 11:42:25 -0500

    unimportant

---
+++ Software Engineering - Platformer/Assets/edgeTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class edgeTest : MonoBehaviour {
+    
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
