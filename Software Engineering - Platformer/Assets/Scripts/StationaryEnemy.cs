722571f17c4e3d56ca07666f0c77a154c95a15aa
physidev <42456287+physidev@users.noreply.github.com>
2019-12-12 08:09:05 -0500

    Level-1 design

--- Software Engineering - Platformer/Assets/Scripts/StationaryEnemy.cs
+++ Software Engineering - Platformer/Assets/Scripts/StationaryEnemy.cs
@@ -2,15 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StationaryEnemy : MonoBehaviour {
+public class StationaryEnemy  : MonoBehaviour { 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject obj;
+
+    public int hp = 10;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter(Collision2D collision, Collider other)
+    {
+        if (other.gameObject.CompareTag("wall"))
+        { //death when touching enemy object
+            Debug.Log("Flip");
+            Flip();
+        }
+        if (other.gameObject.CompareTag("enemy"))
+        {
+            Flip();
+        }
+    }
+    void Flip()
+    { //method to flip sprites x around
+        facingRight = !facingRight;
+        GetComponent<SpriteRenderer>().flipX = true;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        hp -= damage;
+
+        if (hp <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        //Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
 }
